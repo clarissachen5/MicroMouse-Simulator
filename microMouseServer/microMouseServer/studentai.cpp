32127d5ec9974551ed1bfc8454fcbb6dff178d20
clarissachen5 <clarissachen5@gmail.com>
2021-07-31 15:52:24 -0700

    added timesLeft(), timesForward(), and timesRight() functions added if condition for going left when there are three direction paths

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -21,53 +21,119 @@ void microMouseServer::studentAI()
  * void foundFinish();
  * void printUI(const char *mesg);
 */
-    void threePaths() {
-        int goneL = 0; //tracker for # times mouse has gone left path
-        int goneF = 0; //tracker for # times mouse has gone forward path
-        int goneR = 0; //tracker for # times mouse has gone right path
-        if (!isWallLeft() && goneL < goneF && goneL < goneR) { //opportunity to move left
-            turnLeft();
-            moveForward();
-            goneL += 1;
-        } else if (!isWallForward() && goneF < goneR) { //opportunity to move forward
-            moveForward();
-            goneF += 1;
-        } else { //opportunity to move right
-            turnRight();
-            moveForward();
-            goneR += 1;
-    }
-    }
 
-    void onlyOnePath() {
-        if (!isWallLeft() && isWallForward() && isWallRight()) { //only left
-            turnLeft();
-            moveForward();
-        } else if (!isWallForward() && isWallRight()) { //only forward
-            moveForward();
-        } else { //only right
-            turnRight();
-            moveForward();
-        }
-    }
-
-    void noPath() { //mouse has hit a dead end and there are no possible direction paths except to turn around
-        turnLeft();
+// unislanded maze
+    if(!isWallLeft())
+    {
         turnLeft();
         moveForward();
     }
-
-    void main(){
-        if (!isWallLeft() && !isWallForward() && !isWallRight()) {
-            threePaths();
-        } else if (!isWallLeft() || !isWallForward() || !isWallRight()){
-            onlyOnePath();
-        } else {
-            noPath();
-            }
+    else if (!isWallForward())
+    {
+        moveForward();
     }
+    else if (!isWallRight())
+    {
+        turnRight();
+        moveForward();
+    }
+    else
+    {
+        turnRight();
+        turnRight();
+        moveForward();
+    }
+//end of unislanded maze code
+
 
 
 }
+
+int timesLeft() {
+    return 0;
+}
+
+int timesForward() {
+    return 0;
+}
+
+int timesRight() {
+    return 0;
+}
+
+
+if (!isWallLeft() && (timesLeft() <= timesForward() && !isWallForward()) && (timesLeft()<= timesRight() && !isWallRight() ))
+{
+    turnLeft();
+    moveForward();
+}
+
+
+
+
+
+void threePaths()
+{
+    int goneL = 0; //tracker for # times mouse has gone left path
+    int goneF = 0; //tracker for # times mouse has gone forward path
+    int goneR = 0; //tracker for # times mouse has gone right path
+    if (!isWallLeft() && goneL < goneF && goneL < goneR)
+    { //opportunity to move left
+        turnLeft();
+        moveForward();
+        goneL += 1;
+    }
+    else if (!isWallForward() && goneF < goneR)
+    { //opportunity to move forward
+        moveForward();
+        goneF += 1;
+    }
+    else
+    { //opportunity to move right
+        turnRight();
+        moveForward();
+        goneR += 1;
+    }
+}
+
+void twoPaths() {
+    if (!isWallLeft()) {
+        turnLeft();
+        moveForward();
+    } else {
+        moveForward(); //if there is a left wall, there will be no forward wall
+    }
+}
+
+void onlyOnePath() {
+    if (!isWallLeft() && isWallForward() && isWallRight()) { //only left
+        turnLeft();
+        moveForward();
+    } else if (!isWallForward() && isWallRight()) { //only forward
+        moveForward();
+    } else { //only right
+        turnRight();
+        moveForward();
+    }
+}
+
+void noPath() { //mouse has hit a dead end and there are no possible direction paths except to turn around
+    turnLeft();
+    turnLeft();
+    moveForward();
+}
+
+void main(){
+    if (!isWallLeft() && !isWallForward() && !isWallRight()) { //three direction paths
+        threePaths();
+    } else if (!isWallLeft() && !isWallForward() || !isWallLeft() && !isWallRight() || !isWallForward() && !isWallRight()) { //two direction paths
+        twoPaths();
+    } else if (!isWallLeft() || !isWallForward() || !isWallRight()){
+        onlyOnePath();
+    } else {
+        noPath();
+        }
+}
+
 
 
