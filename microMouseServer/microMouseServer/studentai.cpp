5a75e9df439977f768dca87ac62c78da32f2cfdd
clarissachen5 <clarissachen5@gmail.com>
2021-07-31 16:26:36 -0700

    made some edits to conditionals to make code work properly

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -1,6 +1,21 @@
 
 #include "micromouseserver.h"
 
+int timesLeft() {
+    return 0;
+}
+
+int timesForward() {
+    return 0;
+}
+
+int timesRight() {
+    return 0;
+}
+
+
+
+
 void microMouseServer::studentAI()
 {
 /*
@@ -23,20 +38,29 @@ void microMouseServer::studentAI()
 */
 
 // unislanded maze
-    if(!isWallLeft())
+
+    if (!isWallLeft() &&
+       !(timesLeft() >= timesForward() && !isWallForward()) &&
+       !(timesLeft()>= timesRight() && !isWallRight())
+       )
     {
         turnLeft();
         moveForward();
     }
-    else if (!isWallForward())
+
+    else if (!isWallForward() &&
+             !(timesForward() >= timesRight() && !isWallRight())
+             )
     {
         moveForward();
     }
+
     else if (!isWallRight())
     {
         turnRight();
         moveForward();
     }
+
     else
     {
         turnRight();
@@ -50,109 +74,6 @@ void microMouseServer::studentAI()
 }
 
 //islanded maze code
-int timesLeft() {
-    return 0;
-}
-
-int timesForward() {
-    return 0;
-}
-
-int timesRight() {
-    return 0;
-}
-
-
-if (!isWallLeft() && (timesLeft() <= timesForward() && !isWallForward()) && (timesLeft()<= timesRight() && !isWallRight()))
-{
-    turnLeft();
-    moveForward();
-}
-
-else if (!isWallForward() && (timesForward() <= timesRight() && !isWallRight()))
-{
-    moveForward();
-}
-
-else if (!isWallRight())
-{
-    turnRight();
-    moveForward();
-}
-
-else
-{
-    turnRight();
-    turnRight();
-    moveForward();
-}
-
 
 //end of islanded maze code
-
-
-void threePaths()
-{
-    int goneL = 0; //tracker for # times mouse has gone left path
-    int goneF = 0; //tracker for # times mouse has gone forward path
-    int goneR = 0; //tracker for # times mouse has gone right path
-    if (!isWallLeft() && goneL < goneF && goneL < goneR)
-    { //opportunity to move left
-        turnLeft();
-        moveForward();
-        goneL += 1;
-    }
-    else if (!isWallForward() && goneF < goneR)
-    { //opportunity to move forward
-        moveForward();
-        goneF += 1;
-    }
-    else
-    { //opportunity to move right
-        turnRight();
-        moveForward();
-        goneR += 1;
-    }
-}
-
-void twoPaths() {
-    if (!isWallLeft()) {
-        turnLeft();
-        moveForward();
-    } else {
-        moveForward(); //if there is a left wall, there will be no forward wall
-    }
-}
-
-void onlyOnePath() {
-    if (!isWallLeft() && isWallForward() && isWallRight()) { //only left
-        turnLeft();
-        moveForward();
-    } else if (!isWallForward() && isWallRight()) { //only forward
-        moveForward();
-    } else { //only right
-        turnRight();
-        moveForward();
-    }
-}
-
-void noPath() { //mouse has hit a dead end and there are no possible direction paths except to turn around
-    turnLeft();
-    turnLeft();
-    moveForward();
-}
-
-void main(){
-    if (!isWallLeft() && !isWallForward() && !isWallRight()) { //three direction paths
-        threePaths();
-    } else if (!isWallLeft() && !isWallForward() || !isWallLeft() && !isWallRight() || !isWallForward() && !isWallRight()) { //two direction paths
-        twoPaths();
-    } else if (!isWallLeft() || !isWallForward() || !isWallRight()){
-        onlyOnePath();
-    } else {
-        noPath();
-        }
-}
-
-
 
